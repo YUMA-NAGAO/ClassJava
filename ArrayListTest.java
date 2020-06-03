0116f3eb5e1ace3ab77a7c4c2fefc8fc66c76983
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-06-03 14:00:25 +0900

    初めてのJavaを使ったListの使い方

---
+++ ArrayListTest.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("AAA");
+        list.add("BBB");
+        list.add("CCC");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
+
+// javaを使った、簡単なListの使い方
+// https://www.sejuku.net/blog/20355
