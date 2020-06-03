09ffed9bfb8f5149db13ac45eb66903b772c7438
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-06-03 15:09:05 +0900

    関数を用いた記述料の削減を行った。

--- ArrayListSample.java
+++ ArrayListSample.java
@@ -7,8 +7,20 @@ public class ArrayListSample {
         list.add("長泉町");
         list.add("清水町");
         list.add("沼津高専");
+        output(list);
+        list.add("裾野市");
+        output(list);
+        list.add(list.indexOf("沼津市") + 1, "三島市");
+        output(list);
+        list.set(list.indexOf("長泉町"), "ながいずみちょう");
+        output(list);
+        list.remove(list.indexOf("沼津高専"));
+        output(list);
+    }
+
+    public static void output(ArrayList<String> list) {
+        System.out.println(list.size());
         for (int i = 0; i < list.size(); i++) {
-            println()
             System.out.println(list.get(i));
         }
     }
