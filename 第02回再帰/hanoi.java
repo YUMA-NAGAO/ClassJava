f862cdccdb5d6102329475d31c9b500c97484170
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-06-14 10:48:57 +0900

    [add]ハイノの塔の攻略

--- 第02回再帰/hanoi.java
+++ 第02回再帰/hanoi.java
@@ -2,6 +2,19 @@ package 第02回再帰;
 
 public class hanoi {
     public static void main(String[] args) {
+        hanoi(3, "A", "B", "C");
+        // A≡開始棒、B≡目的棒、C≡作業棒とする
+    }
+
+    static void hanoi(int n, String start, String goal, String task) {
+        if (n > 1) {
+            hanoi(n - 1, start, task, goal);
+            System.out.println(n + ":" + start + " - > " + goal);
+            hanoi(n - 1, task, goal, start);
+        } else {
+            System.out.println(n + ":" + start + " - > " + goal);
+
+        }
 
     }
 }
--- 第02回再帰/hanoi.py
+++ 第02回再帰/hanoi.py
@@ -1,15 +1,11 @@
-def hanoi(n,fis,las,tas):
+def hanoi(n,start,goal,task):
     if n>1:
-        hanoi(n-1,fis,tas,las)
-        print(str(n)+":"+fis+" -> "+las)
-        hanoi(n-1,tas,las,fis)
+        hanoi(n-1,start,task,goal)
+        print(str(n)+":"+start+"->"+goal)
+        hanoi(n-1,task,goal,start)
     
     else:
-        print(str(n)+":"+fis+" -> "+las)
-        
-    
-n=4
-
-hanoi(n, "A", "B", "C")
-
+        print(str(n)+":"+start+"->"+goal)
+n=3
+hanoi(n,"A","B","C")
 # A≡開始棒、B≡目的棒、C≡作業棒とする
