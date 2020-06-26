3552ce0f14cb730347c2e128b0ccd984c59632e0
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-06-26 14:21:17 +0900

    Finish

--- 第04回スタックとキュー/QueueApp.java
+++ 第04回スタックとキュー/QueueApp.java
@@ -3,8 +3,7 @@ package 第04回スタックとキュー;
 public class QueueApp {
 
     public static void main(String[] args) {
-        QueueApp tes = new QueueApp();
-        Queue obj = tes.new Queue(5);
+        Queue obj = new Queue(5);
         obj.enqueue(2);
         obj.enqueue(3);
         obj.enqueue(1);
@@ -15,6 +14,7 @@ public class QueueApp {
             System.out.println(obj.peek());
             obj.dequeue();
         }
+        // 差をわかりやすくするために、一文を入れる。
         System.out.println("-------------------------");
 
         obj.enqueue(2);
@@ -22,59 +22,60 @@ public class QueueApp {
         obj.enqueue(1);
         obj.enqueue(722);
 
+        // isEmptyの返り値が空では無いとき、Trueなので、無限ループにするために！を付けて無限ループにした
         while (!obj.isEmpty()) {
             System.out.println(obj.peek());
             obj.dequeue();
         }
     }
 
-    class Queue {
-        // 属性
-        private int maxSize;
-        private int[] queueArray;
-        private int front;
-        private int rear;
-        private int nItems;
+}
 
-        // メソッド
-        public Queue(int n) {
-            maxSize = n;
-            queueArray = new int[maxSize];
-            front = 0;
+class Queue {
+    // 属性
+    private int maxSize;
+    private int[] queueArray;
+    private int front;
+    private int rear;
+    private int nItems;
+
+    // メソッド
+    public Queue(int n) {
+        maxSize = n;
+        queueArray = new int[maxSize];
+        front = 0;
+        rear = -1;
+        nItems = 0;
+    }
+
+    public void enqueue(int dt) {
+        if (rear == maxSize - 1) {
             rear = -1;
-            nItems = 0;
         }
+        queueArray[++rear] = dt;
+        nItems++;
 
-        public void enqueue(int dt) {
-            if (rear == maxSize - 1) {
-                rear = -1;
-            }
-            queueArray[++rear] = dt;
-            nItems++;
+    }
 
+    public int dequeue() {
+        int dt = queueArray[front++];
+        if (front == maxSize) {
+            front = 0;
         }
+        nItems--;
+        return dt;
+    }
 
-        public int dequeue() {
-            int dt = queueArray[front++];
-            if (front == maxSize) {
-                front = 0;
-            }
-            nItems--;
-            return dt;
-        }
+    public int peek() {
+        return queueArray[front];
+    }
 
-        public int peek() {
-            return queueArray[front];
-        }
+    public boolean isEmpty() {
+        return (nItems == 0);
+    }
 
-        public boolean isEmpty() {
-            return (nItems == 0);
-        }
-
-        public boolean isFull() {
-            return (nItems == maxSize);
-        }
-
+    public boolean isFull() {
+        return (nItems == maxSize);
     }
 
 }
--- 第04回スタックとキュー/StackApp.java
+++ 第04回スタックとキュー/StackApp.java
@@ -3,14 +3,14 @@ package 第04回スタックとキュー;
 public class StackApp {
 
     public static void main(String[] args) {
-        StackApp tes = new StackApp();
-        Stack obj = tes.new Stack(5);
+        Stack obj = new Stack(5);
         obj.push(2);
         obj.push(3);
         obj.push(1);
         obj.push(5);
         obj.push(6);
 
+        // isEmptyの返り値が空では無いとき、Trueなので、無限ループにするために！を付けて無限ループにした
         while (!obj.isEmpty()) {
             System.out.println(obj.peek());
             obj.pop();
@@ -18,40 +18,40 @@ public class StackApp {
 
     }
 
-    class Stack {
+}
 
-        // 属性
-        private int maxSize;
-        private int[] stackArray;
-        private int top;
+class Stack {
 
-        // メソッド
-        public Stack(int n) {
-            maxSize = n;
-            stackArray = new int[maxSize];
-            top = -1;
-        }
+    // 属性
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
 
-        public void push(int dt) {
-            stackArray[++top] = dt;
-        }
+    // メソッド
+    public Stack(int n) {
+        maxSize = n;
+        stackArray = new int[maxSize];
+        top = -1;
+    }
 
-        public int pop() {
-            return stackArray[top--];
-        }
+    public void push(int dt) {
+        stackArray[++top] = dt;
+    }
 
-        public int peek() {
-            return stackArray[top];
-        }
+    public int pop() {
+        return stackArray[top--];
+    }
 
-        public boolean isEmpty() {
-            return (top == -1);
-        }
+    public int peek() {
+        return stackArray[top];
+    }
 
-        public boolean isFull() {
-            return (top == maxSize - 1);
-        }
+    public boolean isEmpty() {
+        return (top == -1);
+    }
 
+    public boolean isFull() {
+        return (top == maxSize - 1);
     }
 
 }
