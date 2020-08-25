bf3dabde3531a41e88fcb474899c6d5dd68912d1
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-08-25 18:10:05 +0900

    [edit]見つからなかったものを追加

--- 第11回木/TreeApp.java
+++ 第11回木/TreeApp.java
@@ -2,8 +2,8 @@ package 第11回木;
 
 public class TreeApp {
     public static void main(String[] args) {
-        String[] data = { "Ymamot", "Takahashi", "Suzuki", "Tanaka", "Nakamura", "Watanabe", "Katoh", "Satoh", "Itoh",
-                "kobayashi" };
+        String[] data = { "Ymamot", "Takahashi", "Suzukjkdfpakjdpfai", "Tanaka", "Nakamura", "Watanabe", "Katoh",
+                "Satoh", "Itoh", "helsinki", "kobayashi" };
         Tree t = new Tree(data[0]);
         for (int i = 1; i < data.length; i++) {
             t.add(data[i]);
@@ -11,7 +11,14 @@ public class TreeApp {
         t.print(t.getRoot());
 
         Node tmp = t.search("Tanaka");
+        Node tes = t.search("text");
         if (tmp != null) {
+            System.out.printf("見つかりました。%s id=%d\n", tmp.getText(), tmp.getId());
+
+        } else {
+            System.out.println("見つかりません。");
+        }
+        if (tes != null) {
             System.out.printf("見つかりました。%s id=%d\n", tmp.getText(), tmp.getId());
 
         } else {
